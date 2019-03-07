c3d168abd7aef8194a2a6cb342838824796d35ff
Ram Basnet <rambasnet@gmail.com>
2019-03-06 19:29:57 -0700

    updated memory_segments

--- demo-programs/memory_segments.c
+++ demo-programs/memory_segments.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int global_var;
 int global_initialized_var = 5;
@@ -8,9 +9,11 @@ void function() {
    // notice this variable has the same name as the one in main()
    static int static_initialized_var = 5;
    static_initialized_var += 10;
-   printf("static int static_initialized_var = %d\n", static_initialized_var);
+   printf("%s\n", "Output inside function:");
+   printf("static_initialized_var = %d\n", static_initialized_var);
+   printf("static_initialized_var is at address %p\n", &static_initialized_var);
    printf("stack_var = %d\n", stack_var);
-   printf("the function's stack_var is at address 0x%08x\n", &stack_var);
+   printf("stack_var is at address %p\n", &stack_var);
 }
 
 
@@ -21,26 +24,29 @@ int main() {
    static int static_initialized_var = 5;
 
    heap_var_ptr = (int *) malloc(4);
-
+   printf("%s\n", "output from main function");
+   printf("main is at address: %p\n", main);
+   printf("function is at address: %p\n", function);
    // These variables are in the data segment
-   printf("global_initialized_var is at address 0x%08x\n", &global_initialized_var);
-   printf("static_initialized_var is at address 0x%08x\n\n", &static_initialized_var);
+   printf("global_initialized_var is at address %p\n", &global_initialized_var);
+   printf("static_initialized_var is at address %p\n\n", &static_initialized_var);
 
    // These variables are in the bss segment
-   printf("static_var is at address 0x%08x\n", &static_var);
-   printf("global_var is at address 0x%08x\n\n", &global_var);
+   printf("static_var is at address %p\n", &static_var);
+   printf("global_var is at address %p\n\n", &global_var);
 
    // This variable is in the heap segment
-   printf("heap_var is at address 0x%08x\n\n", heap_var_ptr);
+   printf("heap_var is at address %p\n\n", heap_var_ptr);
 
    // These variables are in the stack segment
-   printf("stack_var is at address 0x%08x\n", &stack_var);
+   printf("stack_var is at address %p\n", &stack_var);
 
    printf("\ncalling function()...\n");   
    function();
 
    // call function again
    printf("\ncalling function() again...\n");
-   // what do you think is the values of static variables in function()
+   // what do you think is the values of static variable in function()?
    function();
+   return 0;
 }  
